1c45f09413cc11d32a84fef53d8d964e6c678d01
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-12-12 12:44:48 +0100

    feat(ex. 5.3): add blog creation form for authenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,90 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
   }, []);
+
+  useEffect(() => {
+    // Define y llama a una función asíncrona dentro del callback.
+    // useEffect no puede ser async
+    const fetchBlogs = async () => {
+      try {
+        // Uso de await dentro de la función asíncrona
+        const blogs = await blogService.getAll();
+        setBlogs(blogs);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+
+    if (user) {
+      fetchBlogs();
+    }
+  }, [user]);
+
+  const blogForm = () => (
+    <form onSubmit={handleBlogFormSubmit}>
+      <h2>create new</h2>
+      <div>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Author:
+          <input
+            type="text"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          url:
+          <input
+            type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </label>
+      </div>
+      <p>
+        <button type="submit">Create</button>
+      </p>
+    </form>
+  );
+
+  const handleBlogFormSubmit = async (event) => {
+    event.preventDefault();
+    const newBlog = {
+      title,
+      author,
+      url,
+    };
+
+    const createdBlog = await blogService.create(newBlog);
+    setBlogs(blogs.concat(createdBlog));
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+  };
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -21,7 +101,7 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password });
       window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
-      console.log(user);
+      blogService.setToken(user.token);
       setUser(user);
       setUsername("");
       setPassword("");
@@ -80,9 +160,12 @@ const App = () => {
             {`${user.username} logged in `}
             <button onClick={handleLogout}>Logout</button>
           </p>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
+          <div>{blogForm()}</div>
+          <div>
+            {blogs.map((blog) => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
+          </div>
         </div>
       )}
     </div>
