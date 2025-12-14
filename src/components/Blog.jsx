4281d8719189389a2470453e65b6b8a3682fb1c6
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-12-14 09:16:09 +0100

    feat(ex. 5.7): add toggleable blog details view

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setShowDetail(!showDetail)}>
+          {showDetail ? "hide" : "view"}
+        </button>
+      </div>
+
+      {showDetail && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
