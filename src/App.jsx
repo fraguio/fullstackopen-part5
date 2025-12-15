dc7c87c13141d0866e6a98c412ea055136c0777c
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-12-15 11:55:21 +0100

    feat(ex. 5.10): sort blogs by number of likes

--- src/App.jsx
+++ src/App.jsx
@@ -120,9 +120,11 @@ const App = () => {
           </Togglable>
 
           <div>
-            {blogs.map((blog) => (
-              <Blog key={blog.id} blog={blog} setBlogs={setBlogs} />
-            ))}
+            {[...blogs]
+              .sort((a, b) => b.likes - a.likes)
+              .map((blog) => (
+                <Blog key={blog.id} blog={blog} setBlogs={setBlogs} />
+              ))}
           </div>
         </div>
       )}
