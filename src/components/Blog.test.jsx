c3e096de5c0d4909e27f9e14c78c144f58fd62b7
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-12-16 06:40:33 +0100

    test(ex. 5.14): add test to show blog url and likes after clicking view button

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('show title and author, but neither URL nor likes by default', () => {
   const blog = {
@@ -34,4 +35,37 @@ test('show title and author, but neither URL nor likes by default', () => {
 
   const likes = screen.queryByText(/likes/i)
   expect(likes).toBeNull()
+})
+
+test('show URL and likes when clicking the view button', async () => {
+  const blog = {
+    title: 'Testing React',
+    author: 'Ada Lovelace',
+    url: 'http://example.com',
+    likes: 10,
+    user: {
+      id: '1',
+      username: 'ada',
+    },
+  }
+
+  const user = {
+    id: '1',
+    username: 'ada',
+  }
+
+  render(
+    <Blog blog={blog} user={user} setBlogs={() => {}} />
+  )
+
+  expect(screen.queryByText('http://example.com')).toBeNull()
+  expect(screen.queryByText(/likes/i)).toBeNull()
+
+  const userEventInstance = userEvent.setup()
+  const button = screen.getByText('view')
+  await userEventInstance.click(button)
+
+  expect(screen.getByText('http://example.com')).toBeInTheDocument()
+  expect(screen.getByText(/likes/i)).toBeInTheDocument()
+
 })
